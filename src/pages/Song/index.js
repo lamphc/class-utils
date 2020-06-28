fd839b3dcd356a435fc29f03b5bfb836bdb59ccf
lamphc <lamphc@live.com>
2020-06-28 18:52:09 +0800

    update songs

--- src/pages/Song/index.js
+++ src/pages/Song/index.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 import css from './index.module.css'
-import Header from '../../components/Header';
+import Header from '../../components/Header'
 
 class Song extends Component {
-  render() {
+  state = {
+    url: localStorage.getItem('g-url') || 'http://music.alang.run'
+  }
+  render () {
     return (
       <div className={css.song}>
         <Header title="点歌台" pos={css.fixed} />
-        <iframe id={css.sync} src="https://music.tql.ink" />
-        <a className={css.gh} target="_blank" href="https://github.com/kasuganosoras/SyncMusic">GitHub-Syncm</a>
+        <iframe title="games" id={css.sync} src={this.state.url} />
+        <a className={css.gh} rel="noopener noreferrer" target="_blank" href="https://github.com/kasuganosoras/SyncMusic">GitHub-Syncm</a>
       </div>
-    );
+    )
   }
 }
 
-export default Song;
+export default Song
